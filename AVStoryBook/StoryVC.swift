8d64821a26e5bce9027467d4b40e40b009f2984d
Minami Munakata <june28munazo@gmail.com>
2018-11-02 09:42:34 -0700

    implement camera action

--- AVStoryBook/StoryVC.swift
+++ AVStoryBook/StoryVC.swift
@@ -7,14 +7,52 @@
 //
 
 import UIKit
+import CoreServices
 
 class StoryVC: UIViewController {
-
+  @IBOutlet weak var storyImageView: UIImageView!
+  
+  @IBOutlet weak var cameraButton: UIBarButtonItem! {
+    didSet {
+      cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+    }
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    // ActionSheet ->
   }
-
-
+  
+  @IBAction func cameraButtonTapped(_ sender: UIBarButtonItem) {
+    print("camera")
+    // 1. create an instance of UIImagePickerController
+    let picker = UIImagePickerController()
+    
+    // 2. setup variables (configuration)
+    picker.sourceType = .camera
+    picker.mediaTypes = [kUTTypeImage as String]
+    picker.allowsEditing = true
+    // 3. to get the photo taken, we need to set the delegate of the picker to self
+    picker.delegate = self
+    // 4. present the picker
+    present(picker, animated: true, completion: nil)
+  }
+  
+  
 }
 
+extension StoryVC: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+  func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+    picker.dismiss(animated: true, completion: nil)
+  }
+  
+  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    // 1. get the photo
+    if let photo = (info[.editedImage] ?? info[.originalImage]) as? UIImage {
+      // 2. display in UIImageView
+      storyImageView.image = photo
+    }
+    // 3. dismiss the picker
+    picker.dismiss(animated: true, completion: nil)
+  }
+}
