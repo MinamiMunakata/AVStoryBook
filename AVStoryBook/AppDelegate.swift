bcfa69b7a07a6ec02386c20ee3da258478dd55ed
Minami Munakata <june28munazo@gmail.com>
2018-11-07 19:24:06 -0800

    fix project name in delegate file

--- AVStoryBook/AppDelegate.swift
+++ AVStoryBook/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -50,7 +51,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
      application to it. This property is optional since there are legitimate
      error conditions that could cause the creation of the store to fail.
      */
-    let container = NSPersistentContainer(name: "CoreDataDemo")
+    let container = NSPersistentContainer(name: "AVStoryBook")
     container.loadPersistentStores(completionHandler: { (storeDescription, error) in
       if let error = error as NSError? {
         // Replace this implementation with code to handle the error appropriately.
